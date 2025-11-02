3116dc6a23e06822510e1793b44db5a7df312698
nagifur <jurasekkocik@gmail.com>
2025-11-02 13:52:25 +0100

    fix: Changed Base link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nagifur.github.io',
-
-});
+  site: 'https://nagifur.github.io',
+  base: '/Axion',
+})
